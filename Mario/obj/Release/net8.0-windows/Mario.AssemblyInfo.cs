01041521683b455d77b6014c5ff3e436fa5a3408
liane04 <lianeduarte04@gmail.com>
2025-03-05 09:58:58 +0000

    tamanho_janela

--- Mario/obj/Release/net8.0-windows/Mario.AssemblyInfo.cs
+++ Mario/obj/Release/net8.0-windows/Mario.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mario")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1f5607383fdde964ecc5fc99dd0d72d727d534")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mario")]
